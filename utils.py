571fe2f442770b24a2c8af36316c7fc6e1bfada0
sabari6203 <109780427+sabari6203@users.noreply.github.com>
2025-04-19 10:39:18 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,16 @@
 import time
 import numpy as np
-import tensorflow as tf
+import torch
 import random
 import os
 
 def set_seed_tf(seed):
     random.seed(seed)
     np.random.seed(seed)
-    tf.random.set_seed(seed)  # Updated to TF 2.x syntax
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
-
 
 class Timer(object):
     def __init__(self, name=''):
@@ -32,33 +33,19 @@ class Timer(object):
 
     def update(self):
         self.current_time = time.time()
-
         self.stage_time = self.current_time - self.last_time
         self.last_time = self.current_time
         self.run_time = self.current_time - self.begin_time
         return self
 
-
 def bpr_neg_samp(uni_users, n_users, support_dict, item_array):
-    """
-    param:
-        uni_users - unique users in training data
-        dict - {uid: array[items]}
-        n_users - sample n users
-        neg_num - n of sample pairs for a user.
-        item_array - sample item in this array.
-    return:
-        ret_array - [uid pos_iid neg_iid] * n_records
-    """
-    pos_items = []
     users = np.random.choice(uni_users, size=n_users, replace=True)
+    pos_items = []
     for user in users:
         pos_candidates = support_dict[user]
         pos_item = random.choice(pos_candidates)
         pos_items.append(pos_item)
-
-    pos_items = np.array(pos_items, dtype=np.int64).flatten()
+    pos_items = np.array(pos_items, dtype=np.int64)
     neg_items = np.random.choice(item_array, len(users), replace=True)
-    ret = np.stack([users, pos_items, neg_items], axis=1)
+    ret = torch.tensor(np.stack([users, pos_items, neg_items], axis=1), dtype=torch.long)
     return ret
-
